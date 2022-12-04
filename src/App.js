d76067a9c060d452b746c94b382d0b50c09dad08
Alexander Antsiferov <wetterkrank@gmail.com>
2022-12-04 01:15:28 +0100

    connect to db, receive updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import * as Realm from "realm-web";
 
-function App() {
+// Create the Application
+
+const app = new Realm.App({ id: "dashboard-ddmhi" });
+
+// Define the App component
+
+const App = () => {
+  // Set state variables
+
+  const [user, setUser] = useState();
+  const [events, setEvents] = useState([]); // This useEffect hook will run only once when the page is loaded
+
+  // This useEffect hook will run only once when the page is loaded
+  useEffect(() => {
+    const login = async () => {
+      // Authenticate anonymously
+      const user = await app.logIn(Realm.Credentials.anonymous());
+      setUser(user); // Connect to the database
+
+      const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+      const collection = mongodb.db("dasbot-meta").collection("answers_total"); // Everytime a change happens in the stream, add it to the list of events
+
+      for await (const change of collection.watch()) {
+        setEvents((events) => [...events, change]);
+      }
+    };
+    login();
+  }, []);
+
+  // Return the JSX that will generate HTML for the page
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!!user && (
+        <div className="App-header">
+          <h1>Connected as user ${user.id}</h1>
+
+          <div>
+            <p>Latest events:</p>
+
+            <table>
+              <thead>
+                <tr>
+                  <td>Operation</td>
+                  <td>Document Key</td>
+                  <td>Full Document</td>
+                </tr>
+              </thead>
+
+              <tbody>
+                {events.map((e, i) => (
+                  <tr key={i}>
+                    <td>{e.operationType}</td>
+                    <td>{e.documentKey._id.toString()}</td>
+                    <td>{JSON.stringify(e.fullDocument)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
