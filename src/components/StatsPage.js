7b8ea146b0a4f9dee2d79123f215a79c0adcc1b3
Aleksandr Antciferov <wetterkrank@gmail.com>
2022-12-17 13:01:34 +0100

    add animation

--- src/components/StatsPage.js
+++ src/components/StatsPage.js
@@ -2,17 +2,35 @@ import * as Realm from "realm-web";
 
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 import Navbar from "./Navbar";
 import Spinner from "./Spinner/Spinner";
 
+const toObject = (input) =>
+  Object.assign(...input.map((elem) => ({ [elem._id]: elem.count })));
+
+const Counter = ({ count }) => {
+  return (
+    <motion.p
+      key={count}
+      className="h3"
+      initial={{ color: "#000000" }}
+      animate={{ color: "#ffffff" }}
+      transition={{
+        ease: "easeOut",
+        duration: 0.3,
+        repeat: 1,
+        repeatType: "reverse",
+      }}
+    >
+      {count}
+    </motion.p>
+  );
+};
+
 const StatsPage = () => {
-  const toObject = (input) =>
-    Object.assign(...input.map((elem) => ({ [elem._id]: elem.count })));
-
   const app = new Realm.App({ id: "dashboard-ddmhi" });
-
-  // Set state variables
   const [answersTotal, setAnswersTotal] = useState(null);
 
   // This useEffect hook will run only once when the page is loaded
@@ -45,29 +63,36 @@ const StatsPage = () => {
     login();
   }, []);
 
-  const Stats = ({ answersTotal }) => (
-    <>
-      <div className="row mb-5">
-        <div className="col-6 text-right text-monospace">
-          <p className="h4 mb-3"><span className="d-none d-md-inline">Correct answers </span>✅</p>
-          <p className="h3"> {answersTotal.correct} </p>
-        </div>
-        <div className="col-6 text-left text-monospace">
-          <p className="h4 mb-3">❌<span className="d-none d-md-inline"> Incorrect answers</span></p>
-          <p className="h3"> {answersTotal.incorrect} </p>
-        </div>
-      </div>
-      <div className="row mx-auto">
-        <Link to={`/`} className="btn btn-light btn-lg">Back</Link>
-      </div>
-    </>
-  );
-
   return (
     <>
       <Navbar />
       <div className="container h-100 d-flex flex-column justify-content-center">
-        {answersTotal ? <Stats answersTotal={answersTotal} /> : <Spinner />}
+        {answersTotal ? (
+          <>
+            <div className="row mb-5">
+              <div className="col-6 text-right text-monospace">
+                <p className="h4 mb-3">
+                  <span className="d-none d-md-inline">Correct answers </span>✅
+                </p>
+                <Counter count={answersTotal.correct} />
+              </div>
+              <div className="col-6 text-left text-monospace">
+                <p className="h4 mb-3">
+                  ❌
+                  <span className="d-none d-md-inline"> Incorrect answers</span>
+                </p>
+                <Counter count={answersTotal.incorrect} />
+              </div>
+            </div>
+            <div className="row mx-auto">
+              <Link to={`/`} className="btn btn-light btn-lg">
+                Back
+              </Link>
+            </div>
+          </>
+        ) : (
+          <Spinner />
+        )}
       </div>
     </>
   );
